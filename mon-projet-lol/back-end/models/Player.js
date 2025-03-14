68acfb178bbfadb49d731f431abed609933e9dc9
Bastien Prompsy <62659819+MrPompom@users.noreply.github.com>
2025-03-14 07:43:52 +0100

    Delete mon-projet-lol/back-end directory

--- mon-projet-lol/back-end/models/Player.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const PlayerSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  games: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }] // Référence aux games jouées
-});
-
-export const Player = mongoose.model('Player', PlayerSchema);
