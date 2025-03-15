5d9d7cfb895236f94d2b83592330f835916ddd71
Bastien Prompsy <bastienprompsy@gmail.com>
2025-03-15 08:36:54 +0100

    cors problem

--- back-end/server.js
+++ back-end/server.js
@@ -10,7 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+import cors from 'cors';
+
+app.use(cors({
+  origin: ['https://calculateur-winrate.vercel.app'], // Autoriser uniquement ton frontend
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 
 // Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI, {
